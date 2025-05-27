03071a19e9828f67d680280fc6761c6dd392eabc
leandropharmativa <leandro@pharmativa.com.br>
2025-05-27 10:01:09 -0300

    Update entrada.js

--- pages/api/entrada.js
+++ pages/api/entrada.js
@@ -1,4 +1,5 @@
 import { google } from 'googleapis'
+import credenciais from '../../../credenciais/credenciais.json'
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).send('Método não permitido')
@@ -8,27 +9,14 @@ export default async function handler(req, res) {
     return res.status(400).json({ erro: 'Dados incompletos' })
   }
 
-try {
-  const cred = process.env.GOOGLE_CREDENTIALS
-
-  console.log('📦 Início da variável cred:', cred?.slice?.(0, 100)) // Mostra os primeiros 100 caracteres
-  let parsed = null
   try {
-    parsed = JSON.parse(cred)
-    console.log('✅ JSON.parse OK. client_email:', parsed.client_email)
-  } catch (parseErr) {
-    console.error('❌ Falha no JSON.parse:', parseErr.message)
-    throw new Error('Falha ao interpretar GOOGLE_CREDENTIALS')
-  }
+    const auth = new google.auth.GoogleAuth({
+      credentials: credenciais,
+      scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+    })
 
-  const auth = new google.auth.GoogleAuth({
-    credentials: parsed,
-    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
-  })
-
-  const sheets = google.sheets({ version: 'v4', auth })
-  const spreadsheetId = process.env.SHEET_ID
-
+    const sheets = google.sheets({ version: 'v4', auth })
+    const spreadsheetId = process.env.SHEET_ID
 
     // 👉 1. Lança em Movimentações
     await sheets.spreadsheets.values.append({
@@ -73,7 +61,6 @@ try {
     }
 
     res.status(200).json({ sucesso: true })
-
   } catch (err) {
     console.error('❌ ERRO COMPLETO:', err)
     res.status(500).json({
